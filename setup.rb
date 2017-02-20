41a0954454106f5aa9cf0dcaf98620812e510c0b
Ryan Weyers <weyers.ryan@gmail.com>
2017-02-20 23:58:41 +1300

    Copied the vm setup from skomobo :)

---
+++ setup.rb
@@ -0,0 +1,21 @@
+
+def yaourt (*packages)
+    install("yaourt", packages)
+end
+
+def pacman (*packages)
+    install("pacman", packages)
+end
+
+def install (manager, packages)
+    manager + " -Syu " + packages.join(" ") + " --noconfirm \n"
+end
+
+def install_haskell_ide()
+    "curl -L https://git.io/haskell-vim-now > /tmp/haskell-vim-now.sh \n bash /tmp/haskell-vim-now.sh"
+end
+
+def provision()
+     pacman("yaourt") + " " + yaourt("par", "stack") + " " + install_haskell_ide()
+     #packages = pacman("ctags", "yaourt", "i3", "i3-wm") + yaourt("par", "stack") + install_haskell_ide()
+end
